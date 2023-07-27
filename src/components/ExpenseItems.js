23ab3a7df4a3b12b4641b83d0d31d307e8d44efd
Vivek Kumar <ui.vivekk@gmail.com>
2023-07-27 16:44:12 +0530

    Default cleanup and created expense-item component

---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ExpenseItems = () => {
+	return (
+		<div>
+			<h1>ExpenseItems</h1>
+			<h2> Food Rs 10 </h2>
+			<h3> Petrol Rs 100 </h3>
+			<h4> Movies Rs 200 </h4>
+		</div>
+	);
+};
+
+export default ExpenseItems;
