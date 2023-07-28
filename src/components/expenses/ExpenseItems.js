bbf6de6b62199850ad5cf3d3aaba6ac0439319fa
Vivek Kumar <ui.vivekk@gmail.com>
2023-07-28 20:19:36 +0530

    Added update amount button

--- src/components/expenses/ExpenseItems.js
+++ src/components/expenses/ExpenseItems.js
@@ -1,26 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../ui/Card";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import "./expense-items.css";
 
 const ExpenseItems = (props) => {
+
+	const [expenseAmount,setExpenseAmount ]= useState(props.amount)
+
 	const expenseDate = props.date;
 	const expenseTitle = props.title;
-	const expenseAmount = props.amount;
 	const locationOfExpenditure = props.location;
-
-
-	const deleteExpense = (index) => {
-		const card = document.querySelectorAll(".expense-item");
-		card[index].style.display = "none";
-	}
 
 	return (
 		<Card className="expense-item">
 	    <ExpenseDate date={expenseDate}/>
 	    <ExpenseDetails amount={expenseAmount} title={expenseTitle} location={locationOfExpenditure} />
-			<button onClick={() => deleteExpense(props.index)}>Delete Expense</button>
+			<button onClick={() => setExpenseAmount(100)}>Update Amount</button>
 		</Card>
 	);
 };
